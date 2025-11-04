4e803b4986713f96d43ee04bf7dc4952c096fb88
sneha <sneha@SNEHA>
2025-11-04 16:21:42 +0530

    first

---
+++ Corejavaprogram/src/com/tnsif/multithreading/Extendingthread.java
@@ -0,0 +1,36 @@
+package com.tnsif.multithreading;
+//demo for extending thread class
+
+class Eclipse extends Thread {
+	public void run() {
+		System.out.println("eclipse id"+" "+Thread.currentThread().getId());
+	}
+}
+class Notepad extends Thread{
+	public void run() {
+		System.out.println("notepad id"+" "+Thread.currentThread().getId());
+	}
+	
+}
+class Onenote extends Thread{
+	public void run() {
+		System.out.println("onenote id"+" "+Thread.currentThread().getId());
+	}
+	
+}
+
+public class Extendingthread{
+	public static void main(String[] args) {
+		Eclipse e= new Eclipse();
+		e.start();
+		Notepad n=new Notepad();
+		n.start();
+		Onenote o=new Onenote();
+		o.start();
+		System.out.println("main method is"+" "+Thread.currentThread().getId());
+	}
+	Eclipse e= new Eclipse();
+	
+	
+	
+}
